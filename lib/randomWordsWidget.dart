62d3a523a1584ff0eeeb0e4387fbd552b5e2803e
Craig A Wiswell <cwiswell63@gmail.com>
2019-07-24 16:47:07 -0400

    Add favorite icon to each row

--- lib/randomWordsWidget.dart
+++ lib/randomWordsWidget.dart
@@ -3,6 +3,7 @@ import 'package:english_words/english_words.dart';
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -51,10 +52,15 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
